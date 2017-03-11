ce972ff4780c5e47e9a590f019b1bae7d2566b2f
Steven Galvin <steven.john.galvin@gmail.com>
2017-03-10 22:15:13 -0800

    update html, css, and js to overhaul the style of the page. add a black and gray color theme and edit the description to offer a more minimalistic feel. create columns and rows to make seperate sections for the user to navigate to. add an images folder containing one ping pong ball PNG that repeats horizontally. use css to hide the image until user submits their number.

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,48 @@ var pingPong = function(number) {
   var pingPongArray = [];
   for (var index = 1; index <= number; index += 1) {
     if (index % 3 === 0 && index % 5 !== 0) {
-      pingPongArray.push("Ping!")
+      pingPongArray.push("Ping!");
     } else if (index % 5 === 0 && index % 3 !== 0) {
-      pingPongArray.push("Pong!")
+      pingPongArray.push("Pong!");
     } else if (index % 15 === 0) {
-      pingPongArray.push("Ping-pong!")
+      pingPongArray.push("Ping-pong!");
     } else {
-      pingPongArray.push(index)
+      pingPongArray.push(index);
     }
   };
   return pingPongArray;
 };
 
+// *** Leaving commented code in to ask a question about it
+
+// var pingPong = function(number) {
+//   var pingPongArray = [];
+//   for (var index = 1; index <= number; index += 1) {
+//     if (index % 15 === 0) {
+//       pingPongArray.push("Ping-pong!");
+//     } else if (index % 5 === 0) {
+//       pingPongArray.push("Pong!");
+//     } else if (index % 3 === 0) {
+//       pingPongArray.push("Ping!");
+//     } else {
+//       pingPongArray.push(index);
+//     }
+//   };
+//   return pingPongArray;
+// };
+
 // UI logic
 
 $(document).ready(function() {
   $('form').submit(function(event) {
+    $('.output ul').html('');
     var number = parseInt($('#userInput').val());
-    var results = pingPong(number)
-    console.log(results);
+    var results = pingPong(number);
     results.forEach(function(output) {
-      $('.output').append("<li>" + output + "</li>")
+      $('#pingBG').show()
+      $('#pingPong').show()
+      $('.output ul').append("<li>" + output + "</li>")
     });
-
     event.preventDefault();
   });
 });
