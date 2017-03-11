cc640a31b2bf8cf33e53f67879683e61100b1392
Steven Galvin <steven.john.galvin@gmail.com>
2017-03-10 18:34:24 -0800

    add js and jquery to replace numbers divisible by 15 with Ping-Pong! and use console log to check it works. update previous code to work with the additions. append results into output.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@
 var pingPong = function(number) {
   var pingPongArray = [];
   for (var index = 1; index <= number; index += 1) {
-    if (index % 3 === 0) {
+    if (index % 3 === 0 && index % 5 !== 0) {
       pingPongArray.push("Ping!")
-    } else if (index % 5 === 0) {
+    } else if (index % 5 === 0 && index % 3 !== 0) {
       pingPongArray.push("Pong!")
+    } else if (index % 15 === 0) {
+      pingPongArray.push("Ping-pong!")
     } else {
       pingPongArray.push(index)
     }
@@ -21,6 +23,9 @@ $(document).ready(function() {
     var number = parseInt($('#userInput').val());
     var results = pingPong(number)
     console.log(results);
+    results.forEach(function(output) {
+      $('.output').append("<li>" + output + "</li>")
+    });
 
     event.preventDefault();
   });
